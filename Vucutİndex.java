b91214f4b7a3f13d2a661af28e09f2c7cc2b20a2
ibrahim halil SOYDAN <116032063+ibrahimhalilsoydan@users.noreply.github.com>
2023-05-26 07:28:11 +0000

    Add files via upload

---
+++ Vucutİndex.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Vucutİndex {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+
+        double kg;
+        double  boy;
+
+        System.out.print("Kilonuzu giriniz :");
+        kg = input.nextDouble();
+
+        System.out.print("Boyunuzu giriniz(metre cinsinden) :");
+        boy = input.nextDouble();
+        if (boy >0 && kg>0){
+            double index =  kg/(boy*boy);
+            System.out.print("Vücut Kitle İndeksiniz : " + index);
+
+        }else {
+            System.out.print("Boy ve Kg 0'dan küçük olamaz");
+        }
+
+
+
+
+    }
+}
