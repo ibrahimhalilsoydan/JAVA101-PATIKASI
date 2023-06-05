f0da191b5c61f9407f7ef5fa2518641c23e61ea3
ibrahim halil SOYDAN <116032063+ibrahimhalilsoydan@users.noreply.github.com>
2023-06-05 07:05:43 +0300

    Add files via upload

---
+++ AsalSayi1_100.java
@@ -0,0 +1,15 @@
+public class AsalSayi1_100 {
+    public static void main(String[] args) {
+
+        int takipSayaci=0;
+        int i,j;
+          for (i=2;i<100;i++){
+              for(j=2; j<i; j++){
+                  if(i%j==0) takipSayaci++;
+              }
+              if (takipSayaci==0) System.out.print(i +" - ");
+              takipSayaci=0;
+          }
+
+        }
+    }
