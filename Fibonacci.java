6863f1a22b8cb336a229b8aad4bb9b3b91b40069
ibrahim halil SOYDAN <116032063+ibrahimhalilsoydan@users.noreply.github.com>
2023-06-06 12:46:18 +0300

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Fibonacci serisinin eleman sayısını girin: ");
+        int n = input.nextInt();
+
+        int first =0;
+        int second=1;
+
+
+        for (int i=2; i<n;i++){
+            int next = first+second;
+
+            System.out.println(first+ " + "+second+ " =" +next);
+            first = second;
+            second = next;
+        }
+
+    }
+}
