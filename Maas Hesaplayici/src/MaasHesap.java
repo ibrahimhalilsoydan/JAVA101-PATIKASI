f5c1bea04c6c9eddb7c064d7518b706a2fee7caf
ibrahim halil SOYDAN <116032063+ibrahimhalilsoydan@users.noreply.github.com>
2023-06-24 10:13:55 +0300

    Add files via upload

---
+++ Maas Hesaplayici/src/MaasHesap.java
@@ -0,0 +1,7 @@
+public class MaasHesap {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Kemal", 2000,45,1985);
+        String print = employee.toString();
+        System.out.println(print);
+    }
+}
